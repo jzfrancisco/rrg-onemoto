ed5980968de268eea1fb9afb452ccb09070306a1
joker7391 <75773388+joker7391@users.noreply.github.com>
2023-02-20 12:47:18 +0800

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,37 @@
 import "@/styles/globals.css";
 import { StoreProvider } from "@/utils/Store";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </StoreProvider>
     </SessionProvider>
   );
 }
+
+function Auth({ children }) {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/unauthorized?message=login required");
+    },
+  });
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  return children;
+}
+
+export default MyApp;
