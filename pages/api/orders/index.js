ab28d2315c83ddfda38a2beea20e9f6646efa9a0
joker7391 <75773388+joker7391@users.noreply.github.com>
2023-02-26 19:17:35 +0800

    first commit

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -7,11 +7,14 @@ const handler = async (req, res) => {
   if (!session) {
     return res.status(401).send("signin required");
   }
+  const { user } = session;
   await db.connect();
-
-  const order = await Order.findById(req.query.id);
-  await db.disconnect();
-  res.send(order);
+  const newOrder = new Order({
+    ...req.body,
+    user: user._id,
+  });
+  const order = await newOrder.save();
+  res.status(201).send(order);
 };
 
 export default handler;
